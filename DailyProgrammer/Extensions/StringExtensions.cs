37ff1975a7541d88ebbc59874ee9a18d2db3527b
Nick <nmccarthy@outlook.com>
2020-02-27 20:20:51 -0500

    smorse easy

---
+++ DailyProgrammer/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Concat(this IEnumerable<string> strings)
+        {
+            return string.Concat(strings);
+        }
+
+        public static string Join(this IEnumerable<string> strings, char separator)
+        {
+            return string.Join(separator, strings);
+        }
+    }
+}
